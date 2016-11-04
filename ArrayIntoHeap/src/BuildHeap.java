f16d023635ec89cf95609f814557943f3933e9c5
tpham1620 <pham1620@uw.edu>
2016-11-03 22:55:58 -0700

    complete ArrayInto Heap

--- ArrayIntoHeap/src/BuildHeap.java
+++ ArrayIntoHeap/src/BuildHeap.java
@@ -1,93 +1,105 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class BuildHeap {
-    private int[] data;
-    private List<Swap> swaps;
+	private int[] data;
+	private List<Swap> swaps;
 
-    private FastScanner in;
-    private PrintWriter out;
+	private FastScanner in;
+	private PrintWriter out;
 
-    public static void main(String[] args) throws IOException {
-        new BuildHeap().solve();
-    }
+	public static void main(String[] args) throws IOException {
+		new BuildHeap().solve();
+	}
 
-    private void readData() throws IOException {
-        int n = in.nextInt();
-        data = new int[n];
-        for (int i = 0; i < n; ++i) {
-          data[i] = in.nextInt();
-        }
-    }
+	private void readData() throws IOException {
+		int n = in.nextInt();
+		data = new int[n];
+		for (int i = 0; i < n; ++i) {
+			data[i] = in.nextInt();
+		}
+	}
 
-    private void writeResponse() {
-        out.println(swaps.size());
-        for (Swap swap : swaps) {
-          out.println(swap.index1 + " " + swap.index2);
-        }
-    }
+	private void writeResponse() {
+		out.println(swaps.size());
+		for (Swap swap : swaps) {
+			out.println(swap.index1 + " " + swap.index2);
+		}
+	}
 
-    private void generateSwaps() {
-      swaps = new ArrayList<Swap>();
-      // The following naive implementation just sorts 
-      // the given sequence using selection sort algorithm
-      // and saves the resulting sequence of swaps.
-      // This turns the given array into a heap, 
-      // but in the worst case gives a quadratic number of swaps.
-      //
-      // TODO: replace by a more efficient implementation
-      for (int i = 0; i < data.length/2; ++i) {
-        for (int j = i + 1; j < data.length; ++j) {
-          if (data[i] > data[j]) {
-            swaps.add(new Swap(i, j));
-            int tmp = data[i];
-            data[i] = data[j];
-            data[j] = tmp;
-          }
-        }
-      }
-    }
+	private void generateSwaps() {
+		swaps = new ArrayList<Swap>();
+		// The following naive implementation just sorts 
+		// the given sequence using selection sort algorithm
+		// and saves the resulting sequence of swaps.
+		// This turns the given array into a heap, 
+		// but in the worst case gives a quadratic number of swaps.
+		//
+		// TODO: replace by a more efficient implementation
 
-    public void solve() throws IOException {
-        in = new FastScanner();
-        out = new PrintWriter(new BufferedOutputStream(System.out));
-        readData();
-        generateSwaps();
-        writeResponse();
-        out.close();
-    }
+		for(int i = data.length/2;i>=0;--i){
+			shiftDown(i);
+		}
+	}
 
-    static class Swap {
-        int index1;
-        int index2;
+	private void shiftDown(int i){
+		int smallChild = 0;
+		if(2*i+2<=data.length-1){
+			smallChild = (data[2*i+1]<data[2*i+2]) ? 2*i+1 : 2*i+2;
+			swap(i,smallChild); 
+			shiftDown(smallChild);
+		}else if(2*i+1 == data.length-1){
+			swap(i,2*i+1);
+			shiftDown(2*i+1);
+		}
+	}
+	private void swap(int i, int j){
+		if (data[i] > data[j]) {
+			swaps.add(new Swap(i, j));
+			int tmp = data[i];
+			data[i] = data[j];
+			data[j] = tmp;
+		}
+	}
+	public void solve() throws IOException {
+		in = new FastScanner();
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		readData();
+		generateSwaps();
+		writeResponse();
+		out.close();
+	}
 
-        public Swap(int index1, int index2) {
-            this.index1 = index1;
-            this.index2 = index2;
-        }
-    }
+	static class Swap {
+		int index1;
+		int index2;
 
-    static class FastScanner {
-        private BufferedReader reader;
-        private StringTokenizer tokenizer;
+		public Swap(int index1, int index2) {
+			this.index1 = index1;
+			this.index2 = index2;
+		}
+	}
 
-        public FastScanner() {
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            tokenizer = null;
-        }
+	static class FastScanner {
+		private BufferedReader reader;
+		private StringTokenizer tokenizer;
 
-        public String next() throws IOException {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                tokenizer = new StringTokenizer(reader.readLine());
-            }
-            return tokenizer.nextToken();
-        }
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in));
+			tokenizer = null;
+		}
 
-        public int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
-    }
+		public String next() throws IOException {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				tokenizer = new StringTokenizer(reader.readLine());
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+	}
 }
